157ada74fc874e8c655cf705502947d25412834d
emersonmrd <emerson.martins.rodrigues@gmail.com>
2026-04-30 15:44:53 -0300

    Adicionado a validação do token antes de carregar a página

--- app/hooks/useAuth.ts
+++ app/hooks/useAuth.ts
@@ -4,12 +4,18 @@ import { useEffect, useState } from "react";
 // Importa hooks usado para manipular a navegação do usuário
 import { useRouter } from "next/navigation";
 
+// Impora a instância do axios configurada para fazer requisições para a API
+import instance from "@/services/api";
+
 export default function useAuth() {
   // Insatancia o objeto router
   const router = useRouter();
 
   // Estado para armazenar a autenticação
   const [authenticated, setAuthenticated] = useState<boolean>(false);
+
+  // Estado para controle de carregamento
+  const [loading, setLoading] = useState<boolean>(true);
 
   // Hook para buscar os dados quando o id estiver disponível
   useEffect(() => {
@@ -20,14 +26,33 @@ export default function useAuth() {
     if (!token) {
       // Redirecionar para o login se não estiver autenticado
       router.push("/login");
-    } else {
-      // Atribuir a situação da autenticação
-      setAuthenticated(true);
     }
+
+    // Verifica a validade do token na API
+    const validateToken = async () => {
+      try {
+        // Fazer a requisição à API
+        await instance.get("/validate-token");
+
+        // Atribuir a situação da autenticação
+        setAuthenticated(true);
+      } catch (error) {
+        // Remove o token inválido enviado pelo usuário
+        localStorage.removeItem("token");
+        // Redirecionar para o login se o token for inválido
+        router.push("/login");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    // Chamar a função validar token
+    validateToken();
   }, []);
 
   // Retornar a situação da autenticação
   return {
     authenticated,
+    loading,
   };
 }
